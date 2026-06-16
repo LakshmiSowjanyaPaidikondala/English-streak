c483b4ba5be6320d1fb030214f5c4fcd04f25549
LakshmiSowjanyaPaidikondala <147790009+LakshmiSowjanyaPaidikondala@users.noreply.github.com>
2026-06-16 07:18:13 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,13 @@
-const CACHE = 'english-streak-v1';
-const FILES = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json'];
+const CACHE = 'english-streak-v2';
+const FILES = [
+  '/English-streak/',
+  '/English-streak/index.html',
+  '/English-streak/style.css',
+  '/English-streak/app.js',
+  '/English-streak/manifest.json',
+  '/English-streak/icons/icon-192.png',
+  '/English-streak/icons/icon-512.png'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
